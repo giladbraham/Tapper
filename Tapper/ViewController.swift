aa51d99798302dd894ef83107fe2fcce0b5337f3
Gilad Braham <giladbraham@gmail.com>
2015-11-08 23:07:22 +0200

    Commit

--- Tapper/ViewController.swift
+++ Tapper/ViewController.swift
@@ -9,15 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //Properties
+    var maxTaps = 0
+    var currentTaps = 0
+    
+    //Outlets
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var txtField: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var tapBtn: UIButton!
+    @IBOutlet weak var tapsLbl: UILabel!
+    
+    @IBAction func onPlayBtnClick(sender: UIButton!){
+        
+        
+        if txtField.text != nil && txtField.text != "" {
+            logoImg.hidden = true
+            playBtn.hidden = true
+            txtField.hidden = true
+            
+            tapBtn.hidden = false
+            tapsLbl.hidden = false
+            
+            
+            maxTaps = Int(txtField.text!)!
+            currentTaps = 0
+            updateTapsLbl()
+            
+            
+            
+            
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onCoinClick(sender: UIButton!){
+        currentTaps++
+        updateTapsLbl()
+        
+        if isGameOver(){
+            resetGame()
+        }
+    }
+    
+    func updateTapsLbl(){
+        tapsLbl.text = "\(currentTaps) Taps"
+    }
+    
+    func isGameOver() -> Bool {
+        return currentTaps >= maxTaps
+       
+        
+    }
+    
+    func resetGame() {
+        maxTaps = 0
+        txtField.text = ""
+        
+        logoImg.hidden = false
+        playBtn.hidden = false
+        txtField.hidden = false
+        
+        tapBtn.hidden = true
+        tapsLbl.hidden = true
     }
 
 
